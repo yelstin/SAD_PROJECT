07b59db5dfa1bc012e563752ac36f19b731599ef
yelstin <Selvarasu.BakkiyaRaja@stud.hochschule-heidelberg.de>
2015-05-12 11:33:49 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+document.getElementById("foot01").innerHTML =
+"<p>&copy;  " + new Date().getFullYear() + " SRH Podcast. All rights reserved.</p>";
+
+	
+document.getElementById("nav01").innerHTML =
+"<ul id='menu'>" +
+"<li><a href='index.html'>Home</a></li>" +
+"<li><a href='customers.html'>Audio</a></li>" +
+"<li><a href='newsfeed.html'>Newsfeed</a></li>" +
+"<li><a href='videos.html'>Videos</a></li>"+
+"<li><a href='srhmusic.html'>SRH Music</a></li>"+
+"<li><a href='joinus.html'>JOIN US</a></li>"+
+"</ul>";
